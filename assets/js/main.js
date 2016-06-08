214b6895364a094d5a3540b785bbc88e3a615815
AnnaRadchenko <i_believe_i_can_fly_@ukr.net>
2016-06-09 00:37:28 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+	/* ----------------------------------------------------------- */
+	/*  1. HEADER CONTENT SLIDE (SLICK SLIDER)
+	/* ----------------------------------------------------------- */
+
+	jQuery('.header-slide').slick({
+		dots: false,
+		infinite: true,
+		speed: 500,
+		arrows:false, 
+		autoplay: true,     
+      	slidesToShow: 1,
+		slide: 'span',
+		fade: true,
+		cssEase: 'linear'
+	});
+
+
+
+/* ----------------------------------------------------------- */
+	/*  6. SCROLL TOP BUTTON
+	/* ----------------------------------------------------------- */
+
+	//Check to see if the window is top if not then display button
+
+	  jQuery(window).scroll(function(){
+	    if ($(this).scrollTop() > 300) {
+	      $('.scrollToTop').fadeIn();
+	    } else {
+	      $('.scrollToTop').fadeOut();
+	    }
+	  });
+	   
+	  //Click event to scroll to top
+
+	  jQuery('.scrollToTop').click(function(){
+	    $('html, body').animate({scrollTop : 0},800);
+	    return false;
+	  });
